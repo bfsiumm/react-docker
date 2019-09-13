7d9b0da1a44b0e1e6d7acc6c48d9772abdf06a31
bfsiumm <147061@supinfo.com>
2019-09-13 15:18:38 +0200

    changed app text

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          ok
+          Changed only in the features branch
         </a>
       </header>
     </div>
